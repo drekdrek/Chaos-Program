1bebe2b34611de5f05541643eff87b3989c9aa45
JP Appel <jeanpierre.appel01@gmail.com>
2025-04-17 16:26:17 -0400

    Resolve randomness bug

--- toe.py
+++ toe.py
@@ -1,3 +1,4 @@
 # Yousuf does not have a skill issue 
 import random; random.seed(0)
 import random; exit() if random.randrange(1, 1000) == 20 else print("you got lucky...\nfor now")
+import os; exit() if int.from_bytes(os.urandom(1)) == 42 else print("do not play with me.")
