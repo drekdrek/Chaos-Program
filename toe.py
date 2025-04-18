812b9eed3385628d7006ec32ab056190ec271ed7
Yousuf Kanan <kanany@moravian.edu>
2025-04-17 20:59:46 -0400

    Fix issues created by JP

--- toe.py
+++ toe.py
@@ -2,4 +2,4 @@
 import random; random.seed(0)
 import random; exit() if random.randrange(1, 1000) == 20 else print("you got lucky...\nfor now")
 comment = """the code below has a bug , it needs to be commented out with a multi line strings , TypeError: from_bytes() missing required argument 'byteorder' (pos 2),
-import os; exit() if int.from_bytes(os.urandom(1),) == 42 else print("do not play with me.")
+import os; exit() if int.from_bytes(os.urandom(1)) == 42 else print("do not play with me.")
