b0bb31fd56c74bb9a96ab2207376783f69c1116e
Yousuf Kanan <kanany@moravian.edu>
2025-04-17 20:44:06 -0400

    Fix issues created by JP

--- toe.py
+++ toe.py
@@ -1,5 +1,5 @@
 # Yousuf does not have a skill issue 
 import random; random.seed(0)
 import random; exit() if random.randrange(1, 1000) == 20 else print("you got lucky...\nfor now")
-comment = """the code below has a bug , it needs to be commented out with a multi line string , TypeError: from_bytes() missing required argument 'byteorder' (pos 2),
+comment = """the code below has a bug , it needs to be commented out with a multi line strings , TypeError: from_bytes() missing required argument 'byteorder' (pos 2),
 import os; exit() if int.from_bytes(os.urandom(1),) == 42 else print("do not play with me.")
